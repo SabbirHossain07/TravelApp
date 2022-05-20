be70201b8423c4e44fbe3269ca8ddda29d470fa7
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-05-20 16:32:05 +0600

    Initial Commit

---
+++ Travel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travel
+//
+//  Created by Sopnil Sohan on 20/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Sopnil Sohan on 20/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
